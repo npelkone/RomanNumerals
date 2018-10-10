4a0989675a0ff0db044917118ff531ff7a46f657
npelkone <niko.pelkonen@hotmail.com>
2018-10-10 18:26:49 +0300

    Assignment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,82 @@
 
 public class RomanNumerals {
+	public String previousString="A";
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+		if (checkRepeatedValues(romanNum) == false) {
+			return 0;
+		} else {
+			String[] splitRomanNum = romanNum.split("");
+			int convertedNumber = 0;
+			
+			for(String str : splitRomanNum) {
+				if(checkThisValue(previousString) < checkThisValue(str)) {
+					convertedNumber -= checkRomanNumber(previousString);
+					convertedNumber -= checkRomanNumber(previousString);
+					convertedNumber += checkRomanNumber(str);
+				}
+				else {
+					convertedNumber += checkRomanNumber(str);
+				}
+			}
+			return convertedNumber;
+		}
+	}
+	
+	public int checkThisValue(String str) {
+		switch(str) {
+			case "M":
+				return 1000;
+			case "D":
+				return 500;
+			case "C":
+				return 100;
+			case "L":
+				return 50;
+			case "X":
+				return 10;
+			case "V":
+				return 5;
+			case "I":
+				return 1;
+			default:
+				return 2000;
+		}
+	}
+	
+	public int checkRomanNumber(String romanNum) {
+		switch(romanNum) {
+			case "M":
+				previousString = romanNum;
+				return 1000;
+			case "D":
+				previousString = romanNum;
+				return 500;
+			case "C":
+				previousString = romanNum;
+				return 100;
+			case "L":
+				previousString = romanNum;
+				return 50;
+			case "X":
+				previousString = romanNum;
+				return 10;
+			case "V":
+				previousString = romanNum;
+				return 5;
+			case "I":
+				previousString = romanNum;
+				return 1;
+			default:
+				return 2000;
+		}
+	}
+	
+	public boolean checkRepeatedValues(String romanNum) {
+		if (romanNum.contains("IIII") || romanNum.contains("XXXX") || romanNum.contains("CCCC") || romanNum.contains("MMMM")) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,75 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_convert_to_integer_xx() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("XX"),20);
 	}
-
+	
+	@Test
+	public void test_convert_to_integer_iiii() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("IIII"),0);
+	}
+	
+	@Test
+	public void test_convert_to_integer_iv() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("IV"),4);
+	}
+	
+	@Test
+	public void test_convert_to_integer_xc() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("XC"),90);
+	}
+	
+	@Test
+	public void test_convert_to_integer_cd() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("CD"),400);
+	}
+	
+	@Test
+	public void test_convert_to_integer_dccc() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("DCCC"),800);
+	}
+	
+	@Test
+	public void test_convert_to_integer_ix() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.convertToInteger("IX"),9);
+	}
+	
+	@Test
+	public void test_check_roman_string_i() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.checkRomanNumber("I"),1);
+	}
+	
+	@Test
+	public void test_check_roman_string_x() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.checkRomanNumber("X"),10);
+	}
+	
+	@Test
+	public void test_check_roman_string_d() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.checkRomanNumber("D"),500);
+	}
+	
+	@Test
+	public void test_check_roman_string_m() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.checkRomanNumber("M"),1000);
+	}
+	
+	@Test
+	public void test_check_roman_string_xd() {
+		RomanNumerals rn = new RomanNumerals();
+		assertEquals(rn.checkRomanNumber("XD"),2000);
+	}
+	
 }
